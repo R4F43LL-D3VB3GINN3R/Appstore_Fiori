41fe43e4ce61076bfacfd77fbe5b72a64cd1f282
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2025-03-18 09:35:44 +0000

    Create catalogo.controller.js

---
+++ catalogo.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("sbx.rla.bc.appstore.controller.catalogo", {
+        onInit: function () {
+            console.log("Controller catalogo carregado");
+        }
+    });
+});
